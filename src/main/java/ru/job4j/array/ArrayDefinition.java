9464f49bb24af0c411f24e194af18a5c8645c611
Andrey <ham2188@mail.ru>
2023-09-09 16:38:52 +0300

    6.0.2. Размер массива [#333582]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -5,5 +5,9 @@ public class ArrayDefinition {
         short[] ages = new short[10];
         String[] surname = new String[100500];
         float[] prices = new float[40];
+
+        System.out.println("Рзмер массива ages: " + ages.length);
+        System.out.println("Рзмер массива surname: " + surname.length);
+        System.out.println("Рзмер массива prices: " + prices.length);
     }
 }
